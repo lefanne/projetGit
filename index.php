a90d6ab01595eb52155d8425e554e67aa47ae8e2
Anne <moulindelapierre@wanadoo.fr>
2019-03-22 12:34:25 +0100

    j'ai changé le titre

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if(isset($_POST["login"])){
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Formulaire modifié</title>
+    <title>NOUVELLE MODIF</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
